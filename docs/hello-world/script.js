f4f952c21a2202d1d6873161b9b7f2361b838cf9
victoriaba92 <vicky.acevedo.bti@gmail.com>
2024-08-08 22:37:50 -0300

    commit hello world

--- docs/hello-world/script.js
+++ docs/hello-world/script.js
@@ -1,1 +1,37 @@
 // write here
+class HelloWorld extends Phaser.Scene {
+    constructor() {
+        super("HelloWorld");
+    }
+
+    preload() {
+        this.load.image("sky", "./assets/space3.png");
+        this.load.image("logo", "./assets/logo.png");
+    }
+
+    create() {
+        this.add.image(400, 300, "sky");
+
+        this.logo = this.physics.add.image(400, 100, "logo");
+
+        this.logo.body.setVelocity(100, 200);
+        this.logo.body.setBounce(1, 1);
+        this.logo.body.setCollideWorldBounds(true);
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 200 },
+            debug: false,
+        },
+    },
+    scene: HelloWorld,
+};
+
+const game = new Phaser.Game(config);
