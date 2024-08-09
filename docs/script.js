81d367e141a9d7c50846d1c1cb4088fe19e12b6c
victoriaba92 <vicky.acevedo.bti@gmail.com>
2024-08-09 00:38:25 -0300

    commit deploy

--- docs/script.js
+++ docs/script.js
@@ -146,6 +146,10 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
     physics: {
         default: "arcade",
         arcade: {
