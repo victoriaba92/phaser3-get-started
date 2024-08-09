4affeb3601e95cad10c3d3d13e52e1c15d7464d7
victoriaba92 <vicky.acevedo.bti@gmail.com>
2024-08-09 00:28:46 -0300

    commit enemy

--- docs/script.js
+++ docs/script.js
@@ -75,6 +75,17 @@ class Game extends Phaser.Scene {
             fontSize: "32px",
             fill: "#000",
         });
+
+
+        this.bombs = this.physics.add.group();
+        this.physics.add.collider(this.bombs, this.platforms);
+        this.physics.add.collider(
+            this.player,
+            this.bombs,
+            this.hitBomb,
+            null,
+            this
+        );
     }
 
     update() {
@@ -101,6 +112,32 @@ class Game extends Phaser.Scene {
 
         this.score += 10;
         this.scoreText.setText(`Score: ${this.score}`);
+
+        if (this.stars.countActive(true) === 0) {
+            this.stars.children.iterate(function(child) {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            var x =
+                player.x < 400 ?
+                Phaser.Math.Between(400, 800) :
+                Phaser.Math.Between(0, 400);
+
+            var bomb = this.bombs.create(x, 16, "bomb");
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
+    }
+
+    hitBomb(player, bomb) {
+        this.physics.pause();
+
+        player.setTint(0xff0000);
+
+        player.anims.play("turn");
+
+        this.gameOver = true;
     }
 
 }
