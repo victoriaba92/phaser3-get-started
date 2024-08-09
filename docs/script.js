3e44cd7014f6e97ebd6e180293ca49122847d7bb
victoriaba92 <vicky.acevedo.bti@gmail.com>
2024-08-08 22:56:35 -0300

    commit player

--- docs/script.js
+++ docs/script.js
@@ -16,7 +16,37 @@ class Game extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, "sky");
-        this.add.image(400, 300, "star");
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, this.platforms);
     }
 }
 
